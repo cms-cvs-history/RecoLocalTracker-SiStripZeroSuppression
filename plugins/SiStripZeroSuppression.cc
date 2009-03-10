6de9deb0ba24cc855bef5b430498f47df0c74581
Burt Betchart <sha1-60bb7eb19bf777a37a7ee89dfcf353857811e301@cern.ch>
2009-03-10 15:50:14 +0000

    Remove unnecessary include

--- plugins/SiStripZeroSuppression.cc
+++ plugins/SiStripZeroSuppression.cc
@@ -3,7 +3,6 @@
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "DataFormats/Common/interface/Handle.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
-#include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "DataFormats/Common/interface/DetSetVector.h"
 #include "DataFormats/SiStripDigi/interface/SiStripDigi.h"
 #include "DataFormats/SiStripDigi/interface/SiStripRawDigi.h"
