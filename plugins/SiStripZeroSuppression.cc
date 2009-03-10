5e445422af6ca7c7101b954f4d72b4e3aedab5f1
Burt Betchart <sha1-60bb7eb19bf777a37a7ee89dfcf353857811e301@cern.ch>
2009-03-10 14:34:10 +0000

    Cleaning plugin

--- plugins/SiStripZeroSuppression.cc
+++ plugins/SiStripZeroSuppression.cc
@@ -1,66 +1,77 @@
 #include "RecoLocalTracker/SiStripZeroSuppression/plugins/SiStripZeroSuppression.h"
+
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "DataFormats/Common/interface/Handle.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
-
 #include "DataFormats/Common/interface/DetSetVector.h"
 #include "DataFormats/SiStripDigi/interface/SiStripDigi.h"
 #include "DataFormats/SiStripDigi/interface/SiStripRawDigi.h"
-
 #include "RecoLocalTracker/SiStripZeroSuppression/interface/SiStripRawProcessingFactory.h"
-
 #include <memory>
 
-SiStripZeroSuppression::SiStripZeroSuppression(edm::ParameterSet const& conf)
+SiStripZeroSuppression::
+SiStripZeroSuppression(edm::ParameterSet const& conf)
   : inputTags(conf.getParameter<std::vector<edm::InputTag> >("RawDigiProducersList")),
     suppressor(SiStripRawProcessingFactory::create_Suppressor(conf)),
     subtractorCMN(SiStripRawProcessingFactory::create_SubtractorCMN(conf)),
-    subtractorPed(SiStripRawProcessingFactory::create_SubtractorPed(conf))
-{
-  for( std::vector<edm::InputTag>::const_iterator  
-	 inputTag = inputTags.begin(); inputTag != inputTags.end(); ++inputTag )
+    subtractorPed(SiStripRawProcessingFactory::create_SubtractorPed(conf))  {
+
+  for(tag_iterator_t inputTag = inputTags.begin(); inputTag != inputTags.end(); ++inputTag )
     produces< edm::DetSetVector<SiStripDigi> > (inputTag->label());
 }
 
-void SiStripZeroSuppression::produce(edm::Event& e, const edm::EventSetup& es)
-{
-  edm::Handle< edm::DetSetVector<SiStripRawDigi> >  input;
-  
-  for( std::vector<edm::InputTag>::const_iterator 
-	 inputTag = inputTags.begin(); inputTag != inputTags.end(); ++inputTag ) {
-    
+void SiStripZeroSuppression::
+produce(edm::Event& e, const edm::EventSetup& es) {
+
+  subtractorPed->init(es);
+  subtractorCMN->init(es);
+  suppressor->init(es);
+
+  for(tag_iterator_t inputTag = inputTags.begin(); inputTag != inputTags.end(); ++inputTag ) {
+
+    std::vector< edm::DetSet<SiStripDigi> > output_base; 
+
+    edm::Handle< edm::DetSetVector<SiStripRawDigi> > input;
     e.getByLabel(*inputTag,input);
-    std::vector< edm::DetSet<SiStripDigi> > vSiStripDigi; 
-    if (input->size()) {
 
-      vSiStripDigi.reserve(10000);
-      subtractorPed->init(es);
-      subtractorCMN->init(es);
-      suppressor->init(es);
+    if (input->size()) 
+      processRaw(*inputTag, *input, output_base);
 
-      for ( edm::DetSetVector<SiStripRawDigi>::const_iterator 
-	      DSViter = input->begin(); DSViter != input->end(); DSViter++) {
-	
-	edm::DetSet<SiStripDigi> ssd(DSViter->id);
-	if ( inputTag->label() == "VirginRaw" ) {
-	  std::vector<int16_t> vssRd(DSViter->data.size());
-	  subtractorPed->subtract(*DSViter,vssRd);
-	  subtractorCMN->subtract(DSViter->id,vssRd);
-	  suppressor->suppress(vssRd,ssd);
-	} 
-	else if ( inputTag->label() == "ProcessedRaw" ){
-	  suppressor->suppress((*DSViter),ssd);	
-	}
-	else throw cms::Exception("Unknown RawDigitype") 
-	  << "SiStripZeroZuppression can only process types \"VirginRaw\" and \"ProcessedRaw\"";
-	
-	if (ssd.data.size())  vSiStripDigi.push_back(ssd); 
-      }
-      
-    }
-    std::auto_ptr< edm::DetSetVector<SiStripDigi> > output(new edm::DetSetVector<SiStripDigi>(vSiStripDigi) );
-    
-    e.put(output,inputTag->label());
+    std::auto_ptr< edm::DetSetVector<SiStripDigi> > output(new edm::DetSetVector<SiStripDigi>(output_base) );
+    e.put( output, inputTag->label() );
   }
-}   
+}
+
+
+inline
+void SiStripZeroSuppression::
+processRaw(const edm::InputTag& inputTag, const edm::DetSetVector<SiStripRawDigi>& input, std::vector<edm::DetSet<SiStripDigi> >& output) {
+  output.reserve(10000);    
+  for ( edm::DetSetVector<SiStripRawDigi>::const_iterator 
+	  rawDigis = input.begin(); rawDigis != input.end(); rawDigis++) {
+    
+
+    edm::DetSet<SiStripDigi> suppressedDigis(rawDigis->id);
+
+    if ( "ProcessedRaw" == inputTag.label()) 
+      suppressor->suppress( *rawDigis, suppressedDigis ); else 
+
+    if ( "VirginRaw" == inputTag.label()) {
+      std::vector<int16_t> processedRawDigis(rawDigis->size());
+      subtractorPed->subtract( *rawDigis, processedRawDigis);
+      subtractorCMN->subtract( rawDigis->id, processedRawDigis);
+      
+      suppressor->suppress( processedRawDigis, suppressedDigis );
+    }
+
+    else throw cms::Exception("Unknown RawDigitype") 
+      << "SiStripZeroZuppression can only process types \"VirginRaw\" and \"ProcessedRaw\"";
+    
+    
+    if (suppressedDigis.size()) 
+      output.push_back(suppressedDigis); 
+
+
+  }
+}
